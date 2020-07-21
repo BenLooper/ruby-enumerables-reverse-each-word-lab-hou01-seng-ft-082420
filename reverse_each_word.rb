65ce1be714342a83587f18f43808bc51525cb7a8
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-21 22:08:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence1)
   final = ''
   word_list = sentence1.split(' ')
-  word_list.each do |word|
+  word_list.collect do |word|
     final.concat(word.reverse + ' ')
   end 
   final.strip 
